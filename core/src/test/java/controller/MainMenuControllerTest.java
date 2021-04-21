5668d970ffd76b5a9149713e239ff12f7d77435d
Tobias Kongsvik <tokongs98@gmail.com>
2021-04-21 14:30:45 +0200

    Fix MainMenuController test

--- core/src/test/java/controller/MainMenuControllerTest.java
+++ core/src/test/java/controller/MainMenuControllerTest.java
@@ -1,47 +1,72 @@
 package controller;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Graphics;
+import com.badlogic.gdx.math.Rectangle;
+import com.esotericsoftware.kryo.Kryo;
 import com.mygdx.progarksurvive.GameState;
 import com.mygdx.progarksurvive.Main;
 import com.mygdx.progarksurvive.controller.MainMenuController;
 import com.mygdx.progarksurvive.model.MainMenuModel;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class MainMenuControllerTest {
 
-    @Test
-    public void testButtons(@Mock MainMenuModel mockModel, @Mock Main mockMain){
-        MainMenuController testController = new MainMenuController(mockModel, mockMain);
-        testController.touchDown(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2 + 30, 0, 0);
-        verify(mockMain, times(1)).setState(GameState.GAME);
+    @Mock MainMenuModel model;
 
-        testController.touchDown(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2 -30, 0, 0);
-        verify(mockMain, times(1)).setState(GameState.SETTINGS);
+    @BeforeAll
+    static void setupBeforeAll(){
+        Gdx.graphics = mock(Graphics.class);
+        when(Gdx.graphics.getHeight()).thenReturn(1080);
+        when(Gdx.graphics.getHeight()).thenReturn(768);
+    }
 
-        testController.touchDown(130, 130, 0,0);
-        verify(mockMain, times(0)).setState(GameState.SETTINGS);
-        verify(mockMain, times(0)).setState(GameState.GAME);
+    @BeforeEach
+    void setup(){
+        when(model.getPlayBtnRect()).thenReturn(new Rectangle(0, 0, 100, 100));
+        when(model.getSettingsBtnRect()).thenReturn(new Rectangle(100, 100, 100, 100));
+    }
+
+    @AfterEach
+    void tearDown(){
+        reset(model);
     }
 
     @Test
-    public void testPlayButton(@Mock MainMenuModel mockModel, @Mock Main mockMain){
-        MainMenuController testController = new MainMenuController(mockModel, mockMain);
-        testController.touchDown(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2 +30,0,1);
-        verify(mockMain, times(1)).setState(GameState.GAME);
+    public void testClickOutsideButtons(@Mock Main game){
+        MainMenuController controller = new MainMenuController(model, game);
+        assertFalse(controller.touchDown(300, 768 - 300, 0,0));
+        verify(game, times(0)).setState(GameState.SETTINGS);
+        verify(game, times(0)).setState(GameState.GAME);
     }
 
     @Test
-    public void testSettingsButton(@Mock MainMenuModel mockModel, @Mock Main mockMain) {
-        MainMenuController testController = new MainMenuController(mockModel, mockMain);
-        testController.touchDown(Gdx.graphics.getWidth()/2, Gdx.graphics.getHeight()/2 -30, 0, 0);
-        verify(mockMain, times(1)).setState(GameState.SETTINGS);
+    public void testClickPlayButton(@Mock Main game){
+        MainMenuController controller = new MainMenuController(model, game);
+        assertFalse(controller.touchDown(300, 768 - 300, 0,0));
+        controller.touchDown(20, 768 - 20, 0, 0);
+        verify(game, times(1)).setState(GameState.GAME);
+        verify(game, times(0)).setState(GameState.SETTINGS);
+    }
+
+    @Test
+    public void testClickSettingsButton(@Mock Main game) {
+        MainMenuController controller = new MainMenuController(model, game);
+        assertFalse(controller.touchDown(300, 768 - 300, 0,0));
+        controller.touchDown(120, 768 - 120, 0, 0);
+        verify(game, times(0)).setState(GameState.GAME);
+        verify(game, times(1)).setState(GameState.SETTINGS);
     }
 
 }
